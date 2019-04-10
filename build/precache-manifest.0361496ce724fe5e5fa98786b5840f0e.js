9624a41a8975b8e9287735a18970e08189e7df28
suke <suke971219@gmail.com>
2019-04-10 22:53:45 +0800

    ./

---
+++ build/precache-manifest.0361496ce724fe5e5fa98786b5840f0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b79fa9789303a3c6740e",
+    "url": "/static/js/main.d3f58800.chunk.js"
+  },
+  {
+    "revision": "27b6c8a5e22f723788e6",
+    "url": "/static/js/2.46789e4c.chunk.js"
+  },
+  {
+    "revision": "b79fa9789303a3c6740e",
+    "url": "/static/css/main.9f5366df.chunk.css"
+  },
+  {
+    "revision": "c87a3c726efc37b27843413759a0839a",
+    "url": "/index.html"
+  }
+];
